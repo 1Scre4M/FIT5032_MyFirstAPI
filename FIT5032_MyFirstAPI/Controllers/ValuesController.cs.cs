02ee41812438f61ac24ed3ee6945a8ec85410dde
1Scre4M <48405361+1Scre4M@users.noreply.github.com>
2022-08-10 19:50:55 +0800

    添加项目文件。

---
+++ FIT5032_MyFirstAPI/Controllers/ValuesController.cs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FIT5032_MyFirstAPI.Controllers
+{
+    public class ValuesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
